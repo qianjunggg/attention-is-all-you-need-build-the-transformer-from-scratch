071af890f1761c4d651fae4d52250772ccced4a0
Yang Junbo <88270088+qianjunggg@users.noreply.github.com>
2026-06-21 18:02:28 +0800

    Step 6: stack_padded_sequences_to_batch

--- model.py
+++ model.py
@@ -67,8 +67,11 @@ def pad_id_sequence(ids, max_len, pad_id):
     
     return ids
 
-# Step 6 - stack_padded_sequences_to_batch (not yet solved)
-# TODO: implement
+# Step 6 - stack_padded_sequences_to_batch
+import torch
+
+def stack_padded_sequences_to_batch(padded_sequences):
+    return torch.tensor(padded_sequences, dtype = torch.long)
 
 # Step 7 - scale_embeddings_by_sqrt_d_model (not yet solved)
 # TODO: implement
